749c593d3a336eefa3b30912d31332e3c043596a
Azarattum <azarattum@pm.me>
2025-03-20 12:32:09 +0700

    Refactored config parsing module

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use evalexpr::{
-    ContextWithMutableVariables, DefaultNumericTypes, HashMapContext, Node, build_operator_tree,
+    ContextWithMutableVariables, HashMapContext, Node, Value as EvalValue, build_operator_tree,
 };
 use serde::Deserialize;
 use serde_json::Value;
@@ -10,200 +10,143 @@ use crate::consts::{
     MODE_FLAG_HAS_RANDOM_COLOR, MODE_FLAG_HAS_SPEED,
 };
 
+type Position = (u8, u8);
+type Range = (u32, u32);
+type Effect = (String, i32, u32);
+
 #[derive(Debug)]
 pub struct Config {
     pub name: String,
     pub vendor_id: u16,
     pub product_id: u16,
-    pub leds: Vec<Option<(u8, (u8, u8))>>,
-    pub effects: Vec<(String, i32, u32)>,
-    pub speed: (u32, u32),
-    pub brightness: (u32, u32),
+    pub leds: Vec<Option<(u8, Position)>>,
+    pub effects: Vec<Effect>,
+    pub speed: Range,
+    pub brightness: Range,
     pub matrix: (u32, u32),
 }
 
 impl Config {
-    pub fn from_str(json_str: &str) -> Result<Config> {
-        let keyboard_json: KeyboardJson = serde_json::from_str(json_str)?;
+    pub fn from_str(json: &str) -> Result<Self> {
+        let KeyboardJson {
+            name,
+            vendor_id,
+            product_id,
+            matrix,
+            menus,
+            layouts,
+        } = serde_json::from_str(json)?;
 
-        let vendor_id = parse_hex(&keyboard_json.vendor_id);
-        let product_id = parse_hex(&keyboard_json.product_id);
-        let matrix = (keyboard_json.matrix.cols, keyboard_json.matrix.rows);
+        let menus = Self::flatten_menus(menus);
 
-        let leds: Vec<_> = keyboard_json
-            .layouts
-            .keymap
+        Ok(Self {
+            name,
+            vendor_id: parse_hex(&vendor_id),
+            product_id: parse_hex(&product_id),
+            matrix: (matrix.cols, matrix.rows),
+            leds: Self::parse_leds(&layouts.keymap),
+            speed: Self::find_range(&menus, "id_qmk_rgb_matrix_effect_speed"),
+            brightness: Self::find_range(&menus, "id_qmk_rgb_matrix_brightness"),
+            effects: Self::parse_effects(menus),
+        })
+    }
+
+    fn parse_leds(keymap: &[Vec<KeymapEntry>]) -> Vec<Option<(u8, Position)>> {
+        keymap
             .iter()
             .flatten()
-            .filter_map(|entry| match entry {
-                KeymapEntry::Key(key) => Some(key),
-                _ => None,
-            })
-            .map(|x| extract_led(x))
-            .collect();
-
-        let menus: Vec<_> = keyboard_json
-            .menus
-            .into_iter()
-            .flat_map(|x| x.content)
-            .flat_map(|x| x.content)
-            .collect();
-
-        // Collect controls
-        let controls: Vec<Control> = menus
-            .iter()
-            .filter_map(|menu_option| match menu_option {
-                MenuOption::Range {
-                    content, show_if, ..
-                } => {
-                    let control_type = match content.get(0).and_then(Value::as_str) {
-                        Some("id_qmk_rgb_matrix_brightness") => Some(ControlType::Brightness),
-                        Some("id_qmk_rgb_matrix_effect_speed") => Some(ControlType::Speed),
-                        _ => None,
-                    };
-                    control_type.map(|control_type| Control {
-                        control_type,
-                        show_if: show_if.clone(),
-                    })
-                }
-                MenuOption::Color { content, show_if } => {
-                    if content.get(0).and_then(Value::as_str) == Some("id_qmk_rgb_matrix_color") {
-                        Some(Control {
-                            control_type: ControlType::Color,
-                            show_if: show_if.clone(),
-                        })
-                    } else {
-                        None
-                    }
-                }
-                MenuOption::ColorPalette { content, show_if } => {
-                    if content.get(0).and_then(Value::as_str) == Some("id_qmk_rgb_matrix_color") {
-                        Some(Control {
-                            control_type: ControlType::ColorPalette,
-                            show_if: show_if.clone(),
-                        })
-                    } else {
-                        None
-                    }
-                }
-                _ => None,
-            })
-            .collect();
-
-        let brightness = menus
-            .iter()
-            .find_map(|x| match x {
-                MenuOption::Range {
-                    content, options, ..
-                } if content
-                    .get(0)
-                    .is_some_and(|x| x == "id_qmk_rgb_matrix_brightness") =>
-                {
-                    Some(*options)
-                }
-                _ => None,
-            })
-            .unwrap_or((0, 0));
-
-        let speed = menus
-            .iter()
-            .find_map(|x| match x {
-                MenuOption::Range {
-                    content, options, ..
-                } if content
-                    .get(0)
-                    .is_some_and(|x| x == "id_qmk_rgb_matrix_effect_speed") =>
-                {
-                    Some(*options)
-                }
-                _ => None,
-            })
-            .unwrap_or((0, 0));
-
-        // Parse effects from the dropdown
-        let effects_dropdown = menus
-            .iter()
-            .find_map(|menu_option| {
-                if let MenuOption::Dropdown {
-                    content, options, ..
-                } = menu_option
-                {
-                    if content.get(0).and_then(Value::as_str) == Some("id_qmk_rgb_matrix_effect") {
-                        Some(options.clone())
-                    } else {
-                        None
-                    }
+            .filter_map(|entry| {
+                if let KeymapEntry::Key(key) = entry {
+                    Some(key)
                 } else {
                     None
                 }
             })
-            .unwrap_or_default();
+            .map(extract_led)
+            .collect()
+    }
 
-        // Compute flags for each effect
-        let effects: Vec<(String, i32, u32)> = effects_dropdown
+    fn flatten_menus(menus: Vec<Menu>) -> Vec<MenuOption> {
+        menus
             .into_iter()
-            .map(|(name, id)| {
-                let mut flags = 0;
-                let mut has_brightness = false;
-                let mut has_speed = false;
-                let mut color_type = None;
+            .flat_map(|x| x.content)
+            .flat_map(|x| x.content)
+            .collect()
+    }
 
-                for control in &controls {
-                    let is_active = match &control.show_if {
-                        Some(show_if) => {
-                            let modified = show_if.replace("{id_qmk_rgb_matrix_effect}", "x");
-                            let expr: Node<_> = match build_operator_tree(&modified) {
-                                Ok(expr) => expr,
-                                Err(_) => continue,
-                            };
-                            let mut context = HashMapContext::<DefaultNumericTypes>::new();
-                            context
-                                .set_value("x".to_string(), evalexpr::Value::Int(id as i64))
-                                .ok();
-                            expr.eval_boolean_with_context(&context).unwrap_or(false)
-                        }
-                        None => true,
-                    };
-
-                    if !is_active {
-                        continue;
-                    }
-
-                    match control.control_type {
-                        ControlType::Brightness => has_brightness = true,
-                        ControlType::Speed => has_speed = true,
-                        ControlType::Color => {
-                            color_type = Some(MODE_FLAG_HAS_MODE_SPECIFIC_COLOR);
-                        }
-                        ControlType::ColorPalette => {
-                            color_type = Some(MODE_FLAG_HAS_PER_LED_COLOR);
-                        }
-                    }
-                }
-
-                if has_brightness {
-                    flags |= MODE_FLAG_HAS_BRIGHTNESS;
-                }
-                if has_speed {
-                    flags |= MODE_FLAG_HAS_SPEED;
-                }
-                let color_flag = color_type.unwrap_or(MODE_FLAG_HAS_RANDOM_COLOR);
-                flags |= color_flag;
-
-                (name, id, flags)
+    fn find_range(menus: &[MenuOption], target: &str) -> Range {
+        menus
+            .iter()
+            .find_map(|m| match m {
+                MenuOption::Range {
+                    content, options, ..
+                } if content.first().and_then(Value::as_str) == Some(target) => Some(*options),
+                _ => None,
             })
-            .collect();
+            .unwrap_or_default()
+    }
 
-        Ok(Config {
-            name: keyboard_json.name,
-            vendor_id,
-            product_id,
-            leds,
-            effects,
-            matrix,
-            brightness,
-            speed,
-        })
+    fn parse_effects(menus: Vec<MenuOption>) -> Vec<Effect> {
+        let controls = Self::collect_controls(&menus);
+
+        menus
+            .into_iter()
+            .find_map(|m| match m {
+                MenuOption::Dropdown { content, options }
+                    if content.first().and_then(Value::as_str)
+                        == Some("id_qmk_rgb_matrix_effect") =>
+                {
+                    Some(options)
+                }
+                _ => None,
+            })
+            .into_iter()
+            .flatten()
+            .map(|(name, id)| {
+                let mut flags = controls
+                    .iter()
+                    .filter(|x| x.is_active(id))
+                    .fold(0, |flags, x| flags | x.flag);
+
+                let has_no_color =
+                    flags & (MODE_FLAG_HAS_PER_LED_COLOR | MODE_FLAG_HAS_MODE_SPECIFIC_COLOR) == 0;
+
+                if has_no_color && id != 0 {
+                    flags = flags | MODE_FLAG_HAS_RANDOM_COLOR;
+                }
+
+                return (name, id, flags);
+            })
+            .collect()
+    }
+
+    fn collect_controls(menus: &[MenuOption]) -> Vec<Control> {
+        menus
+            .iter()
+            .filter_map(|m| match m {
+                MenuOption::Range {
+                    content, show_if, ..
+                } => content
+                    .first()
+                    .and_then(Value::as_str)
+                    .and_then(|id| match id {
+                        "id_qmk_rgb_matrix_brightness" => {
+                            Some(Control::new(show_if, MODE_FLAG_HAS_BRIGHTNESS))
+                        }
+                        "id_qmk_rgb_matrix_effect_speed" => {
+                            Some(Control::new(show_if, MODE_FLAG_HAS_SPEED))
+                        }
+                        _ => None,
+                    }),
+                MenuOption::Color { content, show_if } if is_color_control(content) => {
+                    Some(Control::new(show_if, MODE_FLAG_HAS_MODE_SPECIFIC_COLOR))
+                }
+                MenuOption::ColorPalette { content, show_if } if is_color_control(content) => {
+                    Some(Control::new(show_if, MODE_FLAG_HAS_PER_LED_COLOR))
+                }
+                _ => None,
+            })
+            .collect()
     }
 
     pub fn count_leds(&self) -> u32 {
@@ -216,11 +159,43 @@ impl Config {
     }
 }
 
-fn parse_hex(hex_str: &str) -> u16 {
-    u16::from_str_radix(hex_str.trim_start_matches("0x"), 16).unwrap_or(0)
+#[derive(Debug)]
+struct Control {
+    condition: Option<Node>,
+    flag: u32,
 }
 
-fn extract_led(key: &str) -> Option<(u8, (u8, u8))> {
+impl Control {
+    fn new(expression: &Option<String>, flag: u32) -> Self {
+        Self {
+            flag,
+            condition: expression
+                .as_ref()
+                .and_then(|x| build_operator_tree(x).ok()),
+        }
+    }
+
+    fn is_active(&self, effect_id: i32) -> bool {
+        self.condition.as_ref().map_or(true, |node| {
+            let mut context = HashMapContext::new();
+            let identifier = "{id_qmk_rgb_matrix_effect}";
+            context
+                .set_value(identifier.into(), EvalValue::Int(effect_id.into()))
+                .and_then(|_| node.eval_boolean_with_context(&context))
+                .unwrap_or(false)
+        })
+    }
+}
+
+fn is_color_control(content: &[Value]) -> bool {
+    content.first().and_then(Value::as_str) == Some("id_qmk_rgb_matrix_color")
+}
+
+fn parse_hex(s: &str) -> u16 {
+    u16::from_str_radix(s.trim_start_matches("0x"), 16).unwrap_or(0)
+}
+
+fn extract_led(key: &String) -> Option<(u8, Position)> {
     let mut flags = key.split('\n');
 
     let position: Vec<_> = flags.nth(0)?.split(',').collect();
@@ -245,7 +220,7 @@ fn extract_led(key: &str) -> Option<(u8, (u8, u8))> {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct KeyboardJson {
+struct KeyboardJson {
     name: String,
     #[serde(rename = "vendorId")]
     vendor_id: String,
@@ -256,58 +231,44 @@ pub struct KeyboardJson {
     layouts: Layouts,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum ControlType {
-    Brightness,
-    Speed,
-    Color,
-    ColorPalette,
-}
-
-#[derive(Debug)]
-struct Control {
-    control_type: ControlType,
-    show_if: Option<String>,
-}
-
 #[derive(Debug, Deserialize)]
-pub struct MatrixDimensions {
+struct MatrixDimensions {
     rows: u32,
     cols: u32,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Menu {
+struct Menu {
     content: Vec<MenuContent>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct MenuContent {
+struct MenuContent {
     content: Vec<MenuOption>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
-pub enum MenuOption {
-    #[serde(alias = "range")]
+enum MenuOption {
+    #[serde(rename = "range")]
     Range {
-        options: (u32, u32),
         content: Vec<Value>,
+        options: Range,
         #[serde(rename = "showIf")]
         show_if: Option<String>,
     },
-    #[serde(alias = "dropdown")]
+    #[serde(rename = "dropdown")]
     Dropdown {
         content: Vec<Value>,
         options: Vec<(String, i32)>,
     },
-    #[serde(alias = "color")]
+    #[serde(rename = "color")]
     Color {
         content: Vec<Value>,
         #[serde(rename = "showIf")]
         show_if: Option<String>,
     },
-    #[serde(alias = "color-palette")]
+    #[serde(rename = "color-palette")]
     ColorPalette {
         content: Vec<Value>,
         #[serde(rename = "showIf")]
@@ -319,14 +280,14 @@ pub enum MenuOption {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Layouts {
+struct Layouts {
     keymap: Vec<Vec<KeymapEntry>>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
-pub enum KeymapEntry {
+enum KeymapEntry {
     Key(String),
     #[allow(dead_code)]
-    Group(Value),
+    Other(Value),
 }
